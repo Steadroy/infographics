614476608580eea114b098e675e8359f5bc56cff
Diego Lavayen A <diego.lavayenalarcon@postgrad.manchester.ac.uk>
2015-07-09 15:34:34 +0100

    package.json

---
+++ packages/system/server/models/infographic.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var TemplateSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        default: 'Template'
+    },
+    team: {
+        type: Schema.Types.ObjectId,
+        require: true,
+        ref: 'Team'
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    poster: {
+        type: String,
+        trim: true,
+        default: ''
+    },
+    ready: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    doms: [{
+        type: Schema.Types.ObjectId,
+        require: true,
+        ref: 'Dom'
+    }]
+});
+
+/**
+ * Statics
+ */
+TemplateSchema.statics.load = function (id, cb) {
+    this.findOne({
+        _id: id
+    }).exec(cb);
+};
+
+var deepPopulate = require('mongoose-deep-populate');
+TemplateSchema.plugin(deepPopulate);
+
+mongoose.model('Template', TemplateSchema);
